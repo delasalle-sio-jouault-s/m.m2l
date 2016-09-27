0e4d6cd934d0fcf859dd99763ce747c7a2228665
delasalle-sio-morel-p <delasalle.sio.morel.p@gmail.com>
2016-09-27 15:14:43 +0200

    Patrick ajoute une ligne au fichier test.php

--- test.php
+++ test.php
@@ -1,4 +1,6 @@
 <?php
 // création du fichier par Simon le 27/09/2016 à 15:06:46sc.
 // ajout d'une ligne par Simon le même jour 10sc plus tard
+// ajout d'une ligne par Patrick le même jour mais pas vraiment mais un peu quand même
+// Thibault, tu dois mettre ton commentaire ici ...
 ?>
