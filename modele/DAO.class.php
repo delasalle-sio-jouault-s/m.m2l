dd763844eceb64555d038d2d08522d9305a91345
delasalle-sio-morel-p <delasalle.sio.morel.p@gmail.com>
2016-10-11 15:51:57 +0200

    testerDigicodeBatiment DAO.class.php

--- modele/DAO.class.php
+++ modele/DAO.class.php
@@ -502,8 +502,31 @@ class DAO
 	// Teste si le digicode saisi ($digicodeSaisi) correspond bien à une réservation de salle quelconque
 	// modifié par Patrick le 04/10/2016
 	public function testerDigicodeBatiment($digicodeSaisi)
-	{
-		// Le code ici ...
+{	global $DELAI_DIGICODE;
+		// préparation de la requete de recherche
+		$txt_req = "Select count(*)";
+		$txt_req = $txt_req . " from mrbs_entry, mrbs_entry_digicode";
+		$txt_req = $txt_req . " where mrbs_entry.id = mrbs_entry_digicode.id";
+		$txt_req = $txt_req . " and digicode = :digicodeSaisi";
+		$txt_req = $txt_req . " and (start_time - :delaiDigicode) < " . time();
+		$txt_req = $txt_req . " and (end_time + :delaiDigicode) > " . time();
+		
+		$req = $this->cnx->prepare($txt_req);
+		// liaison de la requête et de ses paramètres
+		$req->bindValue("digicodeSaisi", $digicodeSaisi, PDO::PARAM_STR);	
+		$req->bindValue("delaiDigicode", $DELAI_DIGICODE, PDO::PARAM_INT);	
+				
+		// exécution de la requete
+		$req->execute();
+		$nbReponses = $req->fetchColumn(0);
+		// libère les ressources du jeu de données
+		$req->closeCursor();
+		
+		// fourniture de la réponse
+		if ($nbReponses == 0)
+			return "0";
+		else
+			return "1";
 	}
 	
 	
